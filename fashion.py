3be529764833aa5a55a6dda4689c5a99d67a4a3e
yoshimin86 <tsuchiya@complex.ist.hokudai.ac.jp>
2020-05-20 13:55:20 +0900

    fashion微調整

--- fashion.py
+++ fashion.py
@@ -40,20 +40,19 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(256,10)
        
     def forward(self,x,flag):
-        chan1 = 24
         chan2 = 64
         fil = 5
         bec =int((((28-fil+1)/2)-fil+1)/2 * (((28-fil+1)/2)-fil+1)/2 * chan2)
         #プーリング層
         if flag == 0:
-            x = F.max_pool2d(torch.sigmoid(self.bn1(self.conv1(x))),2)
+            x = F.max_pool2d(F.sigmoid(self.bn1(self.conv1(x))),2)
         else:
-            x = F.max_pool2d(torch.sigmoid(self.conv1(x)),2)
-        x = F.max_pool2d(torch.sigmoid(self.conv2(x)),2)
+            x = F.max_pool2d(F.sigmoid(self.conv1(x)),2)
+        x = F.max_pool2d(F.sigmoid(self.conv2(x)),2)
         if flag==0:
             x = self.dropout1(x)
         x = x.view(-1,bec) #全結合層に入れるために256のベクトルに変換する
-        x = torch.sigmoid(self.fc1(x))
+        x = F.sigmoid(self.fc1(x))
         x = self.dropout2(x)
         x = self.fc2(x)
         return F.log_softmax(x,dim=1)
@@ -121,7 +120,7 @@ for i in range(epochs):
             x = inputs
             y = labels
             output = net(x,flag=1)
-        _, predicted = torch.max(output.data, 1)
+            _,predicted = torch.max(output.data, 1)
         if using_cuda:
             y_predicted = predicted.cpu().numpy()
         else:
